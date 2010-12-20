a1b14a0fe9fa8accb8de4123897c9da6312ff021
Mike Thomas <mike@whatang.org>
2010-12-19 19:03:34 -0500

    Unneeded import.

--- GUI/DBSignals.py
+++ GUI/DBSignals.py
@@ -3,7 +3,7 @@ Created on 7 Dec 2010
 
 @author: Mike Thomas
 '''
-from PyQt4 import QtCore
+
 XSPACING_SIGNAL = "xSpacingChanged"
 YSPACING_SIGNAL = "ySpacingChanged"
 LINESPACING_SIGNAL = "lineSpacingChanged"
