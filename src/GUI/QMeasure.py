fc65b4e5cfc97d76c6bdd104e976d74d7368100b
Mike Thomas <mike@whatang.org>
2011-01-05 00:30:25 -0500

    Create stubs for basic graphic items.

---
+++ src/GUI/QMeasure.py
@@ -0,0 +1,20 @@
+'''
+Created on 5 Jan 2011
+
+@author: Mike Thomas
+
+'''
+
+from PyQt4 import QtGui
+
+class QMeasure(QtGui.QGraphicsItemGroup):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, parent = None):
+        '''
+        Constructor
+        '''
+        super(QMeasure, self).__init__(parent)
---
+++ src/GUI/QMeasureLine.py
@@ -0,0 +1,20 @@
+'''
+Created on 5 Jan 2011
+
+@author: Mike Thomas
+
+'''
+
+from PyQt4 import QtGui
+
+class QMeasureLine(QtGui.QGraphicsItem):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, parent = None):
+        '''
+        Constructor
+        '''
+        super(QMeasureLine, self).__init__(parent)
