b63177a353f917f173056d45c32c577cb1d04de1
Mike Thomas <mike@whatang.org>
2011-01-23 19:06:56 -0500

    Added a bunch of icons.

---
+++ src/GUI/DBIcons.py
@@ -0,0 +1,24 @@
+'''
+Created on 23 Jan 2011
+
+@author: Mike Thomas
+'''
+
+from PyQt4 import QtGui
+
+_ICON_CACHE = {"repeat":"view-refresh",
+               "score":"audio-x-generic",
+               "copy":"edit-copy",
+               "paste":"edit-paste",
+               "delete":"edit-delete"}
+
+
+def initialiseIcons():
+    for iconName, iconLocation in _ICON_CACHE.iteritems():
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(":/Icons/Icons/" + iconLocation + ".png"),
+                       QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        _ICON_CACHE[iconName] = icon
+
+def getIcon(iconName):
+    return _ICON_CACHE[iconName.lower()]
