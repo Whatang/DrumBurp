379874ff7aed9d6a01687ba439b99355023fe840
Mike Thomas <mike@whatang.org>
2011-01-23 20:24:42 -0500

    Set the application task bar icon properly.

--- src/DrumBurp.py
+++ src/DrumBurp.py
@@ -6,14 +6,19 @@ Created on 31 Jul 2010
 import sys
 from PyQt4.QtGui import QApplication
 import GUI.DBMainwindow
+import GUI.DBIcons
 
 def main():
+    import ctypes
+    myappid = 'Whatang.DrumBurp'
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
     app = QApplication(sys.argv)
     app.setOrganizationName("Whatang Software")
     app.setOrganizationDomain("whatang.org")
     app.setApplicationName(GUI.DBMainwindow.APPNAME)
     mainWindow = GUI.DBMainwindow.DrumBurp()
     mainWindow.show()
+    app.setWindowIcon(GUI.DBIcons.getIcon("repeat"))
     app.exec_()
 
 if __name__ == '__main__':
