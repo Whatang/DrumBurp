9c2130305cd8b936d58bc5f275e7f08293cea2b4
Mike Thomas <mike@whatang.org>
2012-12-12 12:47:22 -0500

    Unit tests for ASCII settings.
    For issue #90.

---
+++ src/Data/test/testAsciiSettings.py
@@ -0,0 +1,43 @@
+# Copyright 2011-12 Michael Thomas
+#
+# See www.whatang.org for more information.
+#
+# This file is part of DrumBurp.
+#
+# DrumBurp is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# DrumBurp is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with DrumBurp.  If not, see <http://www.gnu.org/licenses/>
+'''
+Created on 12 Dec 2012
+
+@author: Mike Thomas
+'''
+import unittest
+import Data.ASCIISettings as ASCIISettings
+# pylint: disable-msg=R0904
+
+class TestAsciiSettings(unittest.TestCase):
+    def testNames(self):
+        settings = ASCIISettings.ASCIISettings()
+        names = settings.checkNames()
+        self.assertEqual(names,
+                         ['metadata', 'kitKey', 'omitEmpty', 'underline',
+                          'printCounts', 'emptyLineBeforeSection',
+                          'emptyLineAfterSection'])
+        for name in names:
+            self.assertEqual(getattr(settings, name), True,
+                             "Default value for AsciiSettings.'%s' is wrong"
+                             % name)
+
+
+if __name__ == "__main__":
+    unittest.main()
