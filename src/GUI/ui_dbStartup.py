140e3d40aacd6cbb8f878d2176c806e01d17a11d
mike_vaio <mike@whatang.org>
2015-02-22 12:47:04 +0000

    Fix sizes of widgets containing text.
    Fixes #318.

--- src/GUI/ui_dbStartup.py
+++ src/GUI/ui_dbStartup.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'C:\Users\Mike_2\Eclipse workspace\DrumBurp\src\GUI\dbStartup.ui'
+# Form implementation generated from reading ui file 'C:\Users\mike_000\workspace\DrumBurp\src\GUI\dbStartup.ui'
 #
-# Created: Sat Mar 31 13:52:21 2012
-#      by: PyQt4 UI code generator 4.9.1
+# Created: Sun Feb 22 11:31:19 2015
+#      by: PyQt4 UI code generator 4.10.2
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -12,22 +12,29 @@ from PyQt4 import QtCore, QtGui
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
 except AttributeError:
-    _fromUtf8 = lambda s: s
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
 
 class Ui_dbStartup(object):
     def setupUi(self, dbStartup):
         dbStartup.setObjectName(_fromUtf8("dbStartup"))
-        dbStartup.resize(411, 172)
+        dbStartup.resize(771, 335)
         self.verticalLayout = QtGui.QVBoxLayout(dbStartup)
         self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
-        self.textBrowser = QtGui.QTextBrowser(dbStartup)
-        self.textBrowser.setAutoFillBackground(True)
-        self.textBrowser.setFrameShape(QtGui.QFrame.NoFrame)
-        self.textBrowser.setFrameShadow(QtGui.QFrame.Plain)
-        self.textBrowser.setTextInteractionFlags(QtCore.Qt.LinksAccessibleByKeyboard|QtCore.Qt.LinksAccessibleByMouse)
-        self.textBrowser.setOpenExternalLinks(True)
-        self.textBrowser.setObjectName(_fromUtf8("textBrowser"))
-        self.verticalLayout.addWidget(self.textBrowser)
+        self.label = QtGui.QLabel(dbStartup)
+        self.label.setTextFormat(QtCore.Qt.RichText)
+        self.label.setWordWrap(True)
+        self.label.setTextInteractionFlags(QtCore.Qt.TextBrowserInteraction)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.verticalLayout.addWidget(self.label)
         self.buttonBox = QtGui.QDialogButtonBox(dbStartup)
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
         self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Ok)
@@ -38,17 +45,8 @@ class Ui_dbStartup(object):
         QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("accepted()")), dbStartup.accept)
         QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("rejected()")), dbStartup.reject)
         QtCore.QMetaObject.connectSlotsByName(dbStartup)
-        dbStartup.setTabOrder(self.buttonBox, self.textBrowser)
 
     def retranslateUi(self, dbStartup):
-        dbStartup.setWindowTitle(QtGui.QApplication.translate("dbStartup", "Welcome to DrumBurp", None, QtGui.QApplication.UnicodeUTF8))
-        self.textBrowser.setHtml(QtGui.QApplication.translate("dbStartup", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt; font-weight:600; font-style:italic;\">DrumBurp Copyright (C) 2011-12 Michael Thomas</span></p>\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:8pt;\"></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:8pt;\">This program comes with ABSOLUTELY NO WARRANTY; for details see the </span><span style=\" font-size:8pt; font-weight:600;\">Help&gt;About DrumBurp</span><span style=\" font-size:8pt;\"> menu item. This is free software, and you are welcome to redistribute it under certain conditions; see the licensing information in </span><span style=\" font-size:8pt; font-weight:600;\">Help&gt;About DrumBurp</span><span style=\" font-size:8pt;\"> for details.</span></p>\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:8pt;\"></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:8pt;\">Support and further licensing information is available at </span><a href=\"http://www.whatang.org\"><span style=\" font-size:8pt; text-decoration: underline; color:#0000ff;\">www.whatang.org</span></a></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+        dbStartup.setWindowTitle(_translate("dbStartup", "Welcome to DrumBurp", None))
+        self.label.setText(_translate("dbStartup", "<html><head/><body><p><span style=\" font-size:10pt; font-weight:600; font-style:italic;\">DrumBurp Copyright (C) 2011-15 Michael Thomas</span></p><p><br/></p><p>This program comes with ABSOLUTELY NO WARRANTY; for details see the <span style=\" font-weight:600;\">Help&gt;About DrumBurp</span> menu item. This is free software, and you are welcome to redistribute it under certain conditions; see the licensing information in <span style=\" font-weight:600;\">Help&gt;About DrumBurp</span> for details.</p><p><br/></p><p>Support and further licensing information is available at <a href=\"http://www.whatang.org\"><span style=\" text-decoration: underline; color:#0000ff;\">www.whatang.org</span></a></p></body></html>", None))
 
--- src/Widgets/ui_measureTabs.py
+++ src/Widgets/ui_measureTabs.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'C:\Users\Mike_2\Eclipse workspace\DrumBurp\src\Widgets\measureTabs.ui'
+# Form implementation generated from reading ui file 'C:\Users\mike_000\workspace\DrumBurp\src\Widgets\measureTabs.ui'
 #
-# Created: Sat Mar 31 13:52:26 2012
-#      by: PyQt4 UI code generator 4.9.1
+# Created: Sun Feb 22 11:49:19 2015
+#      by: PyQt4 UI code generator 4.10.2
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -12,33 +12,60 @@ from PyQt4 import QtCore, QtGui
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
 except AttributeError:
-    _fromUtf8 = lambda s: s
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
 
 class Ui_measureTabs(object):
     def setupUi(self, measureTabs):
         measureTabs.setObjectName(_fromUtf8("measureTabs"))
-        measureTabs.resize(218, 155)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Fixed)
+        measureTabs.resize(441, 219)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(measureTabs.sizePolicy().hasHeightForWidth())
         measureTabs.setSizePolicy(sizePolicy)
-        self.gridLayout_2 = QtGui.QGridLayout(measureTabs)
-        self.gridLayout_2.setObjectName(_fromUtf8("gridLayout_2"))
+        self.verticalLayout = QtGui.QVBoxLayout(measureTabs)
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
         self.counterTabs = QtGui.QTabWidget(measureTabs)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.MinimumExpanding, QtGui.QSizePolicy.Fixed)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.counterTabs.sizePolicy().hasHeightForWidth())
         self.counterTabs.setSizePolicy(sizePolicy)
-        self.counterTabs.setMinimumSize(QtCore.QSize(200, 85))
-        self.counterTabs.setMaximumSize(QtCore.QSize(500, 85))
+        self.counterTabs.setMinimumSize(QtCore.QSize(415, 111))
+        self.counterTabs.setMaximumSize(QtCore.QSize(16777215, 16777215))
         self.counterTabs.setTabShape(QtGui.QTabWidget.Triangular)
         self.counterTabs.setObjectName(_fromUtf8("counterTabs"))
         self.simpleTab = QtGui.QWidget()
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.simpleTab.sizePolicy().hasHeightForWidth())
+        self.simpleTab.setSizePolicy(sizePolicy)
         self.simpleTab.setObjectName(_fromUtf8("simpleTab"))
         self.gridLayout = QtGui.QGridLayout(self.simpleTab)
         self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+        spacerItem = QtGui.QSpacerItem(0, 20, QtGui.QSizePolicy.MinimumExpanding, QtGui.QSizePolicy.Minimum)
+        self.gridLayout.addItem(spacerItem, 0, 0, 1, 1)
+        self.beatCountComboBox = QtGui.QComboBox(self.simpleTab)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(1)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.beatCountComboBox.sizePolicy().hasHeightForWidth())
+        self.beatCountComboBox.setSizePolicy(sizePolicy)
+        self.beatCountComboBox.setMinimumSize(QtCore.QSize(0, 0))
+        self.beatCountComboBox.setMaximumSize(QtCore.QSize(200, 16777215))
+        self.beatCountComboBox.setBaseSize(QtCore.QSize(200, 0))
+        self.beatCountComboBox.setObjectName(_fromUtf8("beatCountComboBox"))
+        self.gridLayout.addWidget(self.beatCountComboBox, 1, 2, 1, 1)
         self.label_2 = QtGui.QLabel(self.simpleTab)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Maximum, QtGui.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
@@ -49,11 +76,12 @@ class Ui_measureTabs(object):
         self.label_2.setObjectName(_fromUtf8("label_2"))
         self.gridLayout.addWidget(self.label_2, 0, 1, 1, 1)
         self.beatsSpinBox = QtGui.QSpinBox(self.simpleTab)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(1)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.beatsSpinBox.sizePolicy().hasHeightForWidth())
         self.beatsSpinBox.setSizePolicy(sizePolicy)
+        self.beatsSpinBox.setMaximumSize(QtCore.QSize(200, 16777215))
         self.beatsSpinBox.setBaseSize(QtCore.QSize(100, 0))
         self.beatsSpinBox.setMinimum(1)
         self.beatsSpinBox.setMaximum(256)
@@ -69,12 +97,8 @@ class Ui_measureTabs(object):
         self.label_3.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.label_3.setObjectName(_fromUtf8("label_3"))
         self.gridLayout.addWidget(self.label_3, 1, 1, 1, 1)
-        self.beatCountComboBox = QtGui.QComboBox(self.simpleTab)
-        self.beatCountComboBox.setMinimumSize(QtCore.QSize(0, 0))
-        self.beatCountComboBox.setMaximumSize(QtCore.QSize(200, 16777215))
-        self.beatCountComboBox.setBaseSize(QtCore.QSize(200, 0))
-        self.beatCountComboBox.setObjectName(_fromUtf8("beatCountComboBox"))
-        self.gridLayout.addWidget(self.beatCountComboBox, 1, 2, 1, 1)
+        spacerItem1 = QtGui.QSpacerItem(0, 20, QtGui.QSizePolicy.MinimumExpanding, QtGui.QSizePolicy.Minimum)
+        self.gridLayout.addItem(spacerItem1, 0, 3, 1, 1)
         self.counterTabs.addTab(self.simpleTab, _fromUtf8(""))
         self.complexTab = QtGui.QWidget()
         self.complexTab.setObjectName(_fromUtf8("complexTab"))
@@ -83,19 +107,20 @@ class Ui_measureTabs(object):
         self.complexEditButton = QtGui.QPushButton(self.complexTab)
         self.complexEditButton.setObjectName(_fromUtf8("complexEditButton"))
         self.gridLayout_3.addWidget(self.complexEditButton, 0, 1, 1, 1)
-        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
-        self.gridLayout_3.addItem(spacerItem, 0, 0, 1, 1)
-        spacerItem1 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
-        self.gridLayout_3.addItem(spacerItem1, 0, 2, 1, 1)
+        spacerItem2 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.gridLayout_3.addItem(spacerItem2, 0, 0, 1, 1)
+        spacerItem3 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.gridLayout_3.addItem(spacerItem3, 0, 2, 1, 1)
         self.counterTabs.addTab(self.complexTab, _fromUtf8(""))
-        self.gridLayout_2.addWidget(self.counterTabs, 0, 0, 1, 1)
+        self.verticalLayout.addWidget(self.counterTabs)
         self.groupBox = QtGui.QGroupBox(measureTabs)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Maximum)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.groupBox.sizePolicy().hasHeightForWidth())
         self.groupBox.setSizePolicy(sizePolicy)
-        self.groupBox.setMaximumSize(QtCore.QSize(500, 16777215))
+        self.groupBox.setMinimumSize(QtCore.QSize(415, 66))
+        self.groupBox.setMaximumSize(QtCore.QSize(16777215, 16777215))
         self.groupBox.setBaseSize(QtCore.QSize(200, 0))
         self.groupBox.setObjectName(_fromUtf8("groupBox"))
         self.verticalLayout_2 = QtGui.QVBoxLayout(self.groupBox)
@@ -105,7 +130,9 @@ class Ui_measureTabs(object):
         self.previewText.setTextInteractionFlags(QtCore.Qt.NoTextInteraction)
         self.previewText.setObjectName(_fromUtf8("previewText"))
         self.verticalLayout_2.addWidget(self.previewText)
-        self.gridLayout_2.addWidget(self.groupBox, 1, 0, 1, 1)
+        self.verticalLayout.addWidget(self.groupBox)
+        spacerItem4 = QtGui.QSpacerItem(20, 0, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.MinimumExpanding)
+        self.verticalLayout.addItem(spacerItem4)
         self.label_2.setBuddy(self.beatsSpinBox)
 
         self.retranslateUi(measureTabs)
@@ -113,22 +140,22 @@ class Ui_measureTabs(object):
         QtCore.QMetaObject.connectSlotsByName(measureTabs)
 
     def retranslateUi(self, measureTabs):
-        measureTabs.setWindowTitle(QtGui.QApplication.translate("measureTabs", "Form", None, QtGui.QApplication.UnicodeUTF8))
-        self.simpleTab.setToolTip(QtGui.QApplication.translate("measureTabs", "Edit a simple measure count", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_2.setToolTip(QtGui.QApplication.translate("measureTabs", "The size of each measure in the new score in beats", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_2.setText(QtGui.QApplication.translate("measureTabs", "Beats", None, QtGui.QApplication.UnicodeUTF8))
-        self.beatsSpinBox.setToolTip(QtGui.QApplication.translate("measureTabs", "The size of each measure in the new score in beats", None, QtGui.QApplication.UnicodeUTF8))
-        self.beatsSpinBox.setStatusTip(QtGui.QApplication.translate("measureTabs", "The size of each measure in the new score in beats", None, QtGui.QApplication.UnicodeUTF8))
-        self.beatsSpinBox.setSuffix(QtGui.QApplication.translate("measureTabs", " beats", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_3.setToolTip(QtGui.QApplication.translate("measureTabs", "The number of beats in this measure", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_3.setText(QtGui.QApplication.translate("measureTabs", "Beat count", None, QtGui.QApplication.UnicodeUTF8))
-        self.beatCountComboBox.setToolTip(QtGui.QApplication.translate("measureTabs", "The number of beats in this measure", None, QtGui.QApplication.UnicodeUTF8))
-        self.counterTabs.setTabText(self.counterTabs.indexOf(self.simpleTab), QtGui.QApplication.translate("measureTabs", "Simple Count", None, QtGui.QApplication.UnicodeUTF8))
-        self.complexTab.setToolTip(QtGui.QApplication.translate("measureTabs", "Edit a complex measure count", None, QtGui.QApplication.UnicodeUTF8))
-        self.complexEditButton.setToolTip(QtGui.QApplication.translate("measureTabs", "Edit a complex count", None, QtGui.QApplication.UnicodeUTF8))
-        self.complexEditButton.setText(QtGui.QApplication.translate("measureTabs", "Edit Count", None, QtGui.QApplication.UnicodeUTF8))
-        self.counterTabs.setTabText(self.counterTabs.indexOf(self.complexTab), QtGui.QApplication.translate("measureTabs", "Complex Count", None, QtGui.QApplication.UnicodeUTF8))
-        self.groupBox.setToolTip(QtGui.QApplication.translate("measureTabs", "A preview of the measure count currently selected", None, QtGui.QApplication.UnicodeUTF8))
-        self.groupBox.setTitle(QtGui.QApplication.translate("measureTabs", "Preview", None, QtGui.QApplication.UnicodeUTF8))
-        self.previewText.setText(QtGui.QApplication.translate("measureTabs", "TextLabel", None, QtGui.QApplication.UnicodeUTF8))
+        measureTabs.setWindowTitle(_translate("measureTabs", "Form", None))
+        self.simpleTab.setToolTip(_translate("measureTabs", "Edit a simple measure count", None))
+        self.beatCountComboBox.setToolTip(_translate("measureTabs", "The number of beats in this measure", None))
+        self.label_2.setToolTip(_translate("measureTabs", "The size of each measure in the new score in beats", None))
+        self.label_2.setText(_translate("measureTabs", "Beats", None))
+        self.beatsSpinBox.setToolTip(_translate("measureTabs", "The size of each measure in the new score in beats", None))
+        self.beatsSpinBox.setStatusTip(_translate("measureTabs", "The size of each measure in the new score in beats", None))
+        self.beatsSpinBox.setSuffix(_translate("measureTabs", " beats", None))
+        self.label_3.setToolTip(_translate("measureTabs", "The number of beats in this measure", None))
+        self.label_3.setText(_translate("measureTabs", "Beat count", None))
+        self.counterTabs.setTabText(self.counterTabs.indexOf(self.simpleTab), _translate("measureTabs", "Simple Count", None))
+        self.complexTab.setToolTip(_translate("measureTabs", "Edit a complex measure count", None))
+        self.complexEditButton.setToolTip(_translate("measureTabs", "Edit a complex count", None))
+        self.complexEditButton.setText(_translate("measureTabs", "Edit Count", None))
+        self.counterTabs.setTabText(self.counterTabs.indexOf(self.complexTab), _translate("measureTabs", "Complex Count", None))
+        self.groupBox.setToolTip(_translate("measureTabs", "A preview of the measure count currently selected", None))
+        self.groupBox.setTitle(_translate("measureTabs", "Preview", None))
+        self.previewText.setText(_translate("measureTabs", "TextLabel", None))
 
