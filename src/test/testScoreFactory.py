e836d8a66bf7495c00d029504d388a571e6664c0
mike_vaio <mike@whatang.org>
2015-06-20 06:47:14 +0100

    Copyright notice

--- src/test/testScoreFactory.py
+++ src/test/testScoreFactory.py
@@ -1,8 +1,22 @@
-'''
-Created on Jun 19, 2015
+# Copyright 2015 Michael Thomas
+#
+# See www.whatang.org for more information.
+#
+# This file is part of DrumBurp.
+#
+# DrumBurp is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# DrumBurp is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with DrumBurp.  If not, see <http://www.gnu.org/licenses/>
 
-@author: mike_000
-'''
 import unittest
 import glob
 import os
