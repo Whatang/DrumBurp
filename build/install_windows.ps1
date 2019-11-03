8eb3ed9ed4366552be422b4141fd730ab88e292a
Whatang <mike@whatang.org>
2019-11-03 16:49:28 +0000

    Add PS debug trace.

--- build/install_windows.ps1
+++ build/install_windows.ps1
@@ -1,3 +1,4 @@
+Set-PSDebug -Trace 1
 Set-Item Env:PYTHONIOENCODING UTF-8
 
 # Download and install VS2008 C++ Redistributable
@@ -6,7 +7,7 @@ Invoke-WebRequest https://download.microsoft.com/download/d/2/4/d242c3fb-da5a-45
 & vcredist_x64.exe /quiet /install
 
 Invoke-WebRequest https://netcologne.dl.sourceforge.net/project/pyqt/PyQt4/PyQt-4.11.3/PyQt4-4.11.3-gpl-Py2.7-Qt4.8.6-x64.exe -OutFile pyqt_installer.exe
-& pqyt_installer.exe /S
+& pyqt_installer.exe /S
 
 # Install NSIS installer tool
 Invoke-WebRequest "https://downloads.sourceforge.net/project/nsis/NSIS%203/3.04/nsis-3.04-setup.exe?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fnsis%2Ffiles%2FNSIS%25203%2F3.04%2Fnsis-3.04-setup.exe%2Fdownload%3Fuse_mirror%3Dnetcologne%26download%3D&ts=1572714477" -OutFile nsis-setup.exe
