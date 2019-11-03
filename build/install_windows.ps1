be8bfa342ddb7bc5ea9302f4c61627430042b527
Whatang <mike@whatang.org>
2019-11-03 17:14:27 +0000

    Refactor as many install steps as possible into GitHub action steps.

--- build/install_windows.ps1
+++ build/install_windows.ps1
@@ -2,11 +2,10 @@ Set-PSDebug -Trace 1
 Set-Item Env:PYTHONIOENCODING UTF-8
 
 # Download and install VS2008 C++ Redistributable
-
 choco install vcredist2008
 
-Invoke-WebRequest "https://netcologne.dl.sourceforge.net/project/pyqt/PyQt4/PyQt-4.11.3/PyQt4-4.11.3-gpl-Py2.7-Qt4.8.6-x64.exe" -OutFile pyqt_installer.exe
-& ./pyqt_installer.exe /S
+# Download and install PyQt
+& ./install_pyqt.ps1
 
 # Install NSIS installer tool
 choco install nsis
